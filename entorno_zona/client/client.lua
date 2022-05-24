fc0b970d660d332a10bcd8f83ce4abd50644d64b
Iván Rica <cadox8@gmail.com>
2022-05-24 23:27:30 +0200

    Arreglo no carga en legacy

--- entorno_zona/client/client.lua
+++ entorno_zona/client/client.lua
@@ -3,7 +3,9 @@ ESX = nil
 local data = {}
 local insideZone = -1
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
+end)
 
 TriggerEvent('chat:addSuggestion', '/zona', 'Crea un entorno de zona /zona [distancia] [mensaje]', {})
 TriggerEvent('chat:addSuggestion', '/banda', 'Crea un entorno de zona de bandas /banda [distancia] [mensaje]', {})
@@ -11,114 +13,110 @@ TriggerEvent('chat:addSuggestion', '/borarzona', 'Borra el entorno de zona /borr
 TriggerEvent('chat:addSuggestion', '/reloadzona', 'Recarga los entornos de zona para ti', {})
 
 RegisterCommand("zona", function(source, args, rawCommand)
-  if args[1] == nil or args[2] == nil then
-    TriggerEvent('chat:addMessage', {
-      color = { 255, 66, 66 },
-      multiline = true,
-      args = { 'Entorno Zonas > ', ' Uso: /zona [distancia] [mensaje]' }
-    })
-  else
-    local playerCoords = GetEntityCoords(PlayerPedId())
-    TriggerServerEvent('zona:createZone', args[1], 0, concat(args), playerCoords)
-  end
+    if args[1] == nil or args[2] == nil then
+        TriggerEvent('chat:addMessage', {
+            color = { 255, 66, 66 },
+            multiline = true,
+            args = { 'Entorno Zonas > ', ' Uso: /zona [distancia] [mensaje]' }
+        })
+    else
+        local playerCoords = GetEntityCoords(PlayerPedId())
+        TriggerServerEvent('zona:createZone', args[1], 0, concat(args), playerCoords)
+    end
 end, false)
 
 RegisterCommand("banda", function(source, args, rawCommand)
-  if args[1] == nil or args[2] == nil then
-    TriggerEvent('chat:addMessage', {
-      color = { 255, 66, 66 },
-      multiline = true,
-      args = { 'Entorno Zonas > ', ' Uso: /banda [distancia] [mensaje]' }
-    })
-  else
-    local playerCoords = GetEntityCoords(PlayerPedId())
-    TriggerServerEvent('zona:createZone', args[1], 1, concat(args), playerCoords)
-  end
+    if args[1] == nil or args[2] == nil then
+        TriggerEvent('chat:addMessage', {
+            color = { 255, 66, 66 },
+            multiline = true,
+            args = { 'Entorno Zonas > ', ' Uso: /banda [distancia] [mensaje]' }
+        })
+    else
+        local playerCoords = GetEntityCoords(PlayerPedId())
+        TriggerServerEvent('zona:createZone', args[1], 1, concat(args), playerCoords)
+    end
 end, true)
 
 RegisterCommand("borrarzona", function(source, args, rawCommand)
-  TriggerServerEvent('zona:deteleZone', args[1])
+    TriggerServerEvent('zona:deteleZone', args[1])
 end, true)
 
 RegisterCommand("reloadzona", function(source, args, rawCommand)
-  TriggerServerEvent('zona:loadData')
+    TriggerServerEvent('zona:loadData')
 end, false)
 
 --
 Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(0)
+    while true do
+        Citizen.Wait(0)
 
-    if data ~= nil then
-      for i,v in ipairs(data) do
-        local zone = data[i]
-        local pos = GetEntityCoords(PlayerPedId())
-        if pos ~= nil then
-          local distance = #(pos - zone.coords)
-          if insideZone == -1 then
-            if distance <= (tonumber(zone.distance) or 20) then
-              TriggerEvent('chat:addMessage', {
-                color = { 5, 255, 255 },
-                multiline = true,
-                args = { 'Entorno de Zona [' .. zone.id ..']: ' .. zone.msg }
-              })
-              insideZone = zone.id
+        if data ~= nil then
+            for i, v in ipairs(data) do
+                local zone = data[i]
+                local pos = GetEntityCoords(PlayerPedId())
+                if pos ~= nil then
+                    local distance = #(pos - zone.coords)
+                    if insideZone == -1 then
+                        if distance <= (tonumber(zone.distance) or 20) then
+                            TriggerEvent('chat:addMessage', {
+                                color = { 5, 255, 255 },
+                                multiline = true,
+                                args = { 'Entorno de Zona [' .. zone.id .. ']: ' .. zone.msg }
+                            })
+                            insideZone = zone.id
+                        end
+                    else
+                        if insideZone == zone.id and (distance > (tonumber(zone.distance) or 20)) then
+                            insideZone = -1
+                        end
+                    end
+                end
             end
-          else
-            if insideZone == zone.id and (distance > (tonumber(zone.distance) or 20)) then
-              insideZone = -1
-            end
-          end
+        else
+            TriggerServerEvent('zona:loadData')
+            Citizen.Wait(1000)
         end
-      end
+        Citizen.Wait(1500)
     end
-    Citizen.Wait(1500)
-  end
 end)
 --
 
 AddEventHandler('playerSpawned', function(spawn)
-  Citizen.Wait(7000)
-  print('Loading Zone Data')
-  TriggerServerEvent('zona:loadData')
+    Citizen.Wait(7000)
+    TriggerServerEvent('zona:loadData')
 end)
 
 RegisterNetEvent('zona:sendData')
 AddEventHandler('zona:sendData', function(locations)
-  data = {}
-  for i,v in ipairs(data) do
-    print(data[i].id)
-  end
-  for i,v in ipairs(locations) do
-    table.insert(data, {
-      id = locations[i].id,
-      distance = locations[i].distance,
-      coords = vector3(locations[i].x, locations[i].y, locations[i].z),
-      msg = locations[i].msg
-    })
-  end
-  for i,v in ipairs(data) do
-    print(data[i].id)
-  end
+    data = {}
+    for i, v in ipairs(locations) do
+        table.insert(data, {
+            id = locations[i].id,
+            distance = locations[i].distance,
+            coords = vector3(locations[i].x, locations[i].y, locations[i].z),
+            msg = locations[i].msg
+        })
+    end
 end)
 
 RegisterNetEvent('zona:addData')
 AddEventHandler('zona:addData', function(insertId, distance, msg, coords)
-  table.insert(data, {
-    id = insertId,
-    distance = distance,
-    coords = coords,
-    msg = msg
-  })
+    table.insert(data, {
+        id = insertId,
+        distance = distance,
+        coords = coords,
+        msg = msg
+    })
 end)
 
 -- Utils
 function concat(args)
-  local i = 2
-  local s = ''
-  while args[i] do
-    s = s .. ' ' .. args[i]
-    i = i + 1
-  end
-  return s
+    local i = 2
+    local s = ''
+    while args[i] do
+        s = s .. ' ' .. args[i]
+        i = i + 1
+    end
+    return s
 end
--- entorno_zona/server/server.lua
+++ entorno_zona/server/server.lua
@@ -1,63 +1,65 @@
 ESX = nil
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
+end)
 
 RegisterNetEvent("zona:createZone")
 AddEventHandler("zona:createZone", function(distance, type, msg, coords)
-  local from = source
-  local username = GetPlayerName(source)
-  MySQL.Async.insert('insert into zona (username, distance, type, x, y, z, msg) values (@username, @distance, @type, @x, @y, @z, @msg)',
-    { ['username'] = username, ['distance'] = distance, ['type'] = type, ['x'] = coords.x, ['y'] = coords.y, ['z'] = coords.z, ['msg'] = msg },
-    function(insertId)
-      TriggerClientEvent('zona:addData', -1, insertId, distance, msg, coords)
-      TriggerClientEvent('chat:addMessage', from, {
-        color = { 5, 255, 255 },
-        multiline = true,
-        args = { 'Entorno de Zona creado con la id [' .. insertId .. '] para un rango de ' .. distance .. ' metros.'  }
-      })
-    end
-  )
+    local from = source
+    local username = GetPlayerName(source)
+    MySQL.Async.insert('insert into zona (username, distance, type, x, y, z, msg) values (@username, @distance, @type, @x, @y, @z, @msg)',
+            { ['username'] = username, ['distance'] = distance, ['type'] = type, ['x'] = coords.x, ['y'] = coords.y, ['z'] = coords.z, ['msg'] = msg },
+            function(insertId)
+                TriggerClientEvent('zona:addData', -1, insertId, distance, msg, coords)
+                TriggerClientEvent('chat:addMessage', from, {
+                    color = { 5, 255, 255 },
+                    multiline = true,
+                    args = { 'Entorno de Zona creado con la id [' .. insertId .. '] para un rango de ' .. distance .. ' metros.' }
+                })
+            end
+    )
 end)
 
 RegisterNetEvent('zona:deteleZone')
 AddEventHandler('zona:deteleZone', function(id)
-  local s = source -- Ok?
-  local realId = tonumber(id)
+    local s = source -- Ok?
+    local realId = tonumber(id)
 
-  if realId then
-    MySQL.Async.execute('delete from zona where id = @id', {
-      ['@id'] = realId
-    }, function(result)
-      TriggerClientEvent('chat:addMessage', s, {
-        color = { 255, 66, 66 },
-        multiline = true,
-        args = { 'Entorno Zona', ' Borrado el entorno con la ID ' .. id  }
-      })
-      MySQL.Async.fetchAll('select * from zona', {}, function(result)
-        if result ~= nil then
-          TriggerClientEvent('zona:sendData', -1, s, result)
-        else
-          TriggerClientEvent('zona:sendData', -1, s, {})
-        end
-      end)
-    end)
-  else
-    TriggerClientEvent('chat:addMessage', s, {
-      color = { 255, 66, 66 },
-      multiline = true,
-      args = { 'Entorno Zona', ' No existe la id ' .. id .. '. Comprueba que es un número'  }
-    })
-  end
+    if realId then
+        MySQL.Async.execute('delete from zona where id = @id', {
+            ['@id'] = realId
+        }, function(result)
+            TriggerClientEvent('chat:addMessage', s, {
+                color = { 255, 66, 66 },
+                multiline = true,
+                args = { 'Entorno Zona', ' Borrado el entorno con la ID ' .. id }
+            })
+            MySQL.Async.fetchAll('select * from zona', {}, function(data)
+                if data ~= nil then
+                    TriggerClientEvent('zona:sendData', -1, s, data)
+                else
+                    TriggerClientEvent('zona:sendData', -1, s, {  })
+                end
+            end)
+        end)
+    else
+        TriggerClientEvent('chat:addMessage', s, {
+            color = { 255, 66, 66 },
+            multiline = true,
+            args = { 'Entorno Zona', ' No existe la id ' .. id .. '. Comprueba que es un número' }
+        })
+    end
 end)
 
 RegisterNetEvent('zona:loadData')
 AddEventHandler('zona:loadData', function()
-  local s = source -- Ok?
-  MySQL.Async.fetchAll('select * from zona', {}, function(result)
-    if result ~= nil then
-      TriggerClientEvent('zona:sendData', s, result)
-    else
-      TriggerClientEvent('zona:sendData', s, {})
-    end
-  end)
+    local s = source -- Ok?
+    MySQL.Async.fetchAll('select * from zona', {}, function(result)
+        if result ~= nil then
+            TriggerClientEvent('zona:sendData', s, result)
+        else
+            TriggerClientEvent('zona:sendData', s, { })
+        end
+    end)
 end)
